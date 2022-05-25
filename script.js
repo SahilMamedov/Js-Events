e986ebaf38fb60adde66e85ec031e6b90027e2b9
SahilMamedov <sahilim@code.edu.az>
2022-05-25 16:28:33 +0400

    Feat: event function

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 let container = document.getElementById("container")
 let marginLeftRigth=0;
 let marginTopButtom =0;
+
+document.addEventListener("keydown",function(event){
+
+    if(event.keyCode===37){
+        marginLeftRigth-=2;
+        container.style.marginLeft=`${marginLeftRigth}px`;
+        
+     }
+    if(event.keyCode===38){
+        marginTopButtom-=2;
+        container.style.marginTop=`${marginTopButtom}px`;
+        
+    }
+    if(event.keyCode===39){
+        marginLeftRigth+=2;
+        container.style.marginLeft=`${marginLeftRigth}px`;
+        
+    }
+    if(event.keyCode===40){
+        marginTopButtom+=2;
+        container.style.marginTop=`${marginTopButtom}px`;
+        
+    }
+    })
